a0a99bc2451669989991b5d24964c2398b6327ad
abdullahkhan2001 <93311167+abdullahkhan2001@users.noreply.github.com>
2021-12-21 18:44:23 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+
+} from "react-router-dom";
+import FirebaseConfig from "./Firebase/firebaseConfig"
+import firebase from 'firebase'
+import SignIn from './Pages/SignIn';
+import AdminDashboard from './Pages/AdminDashBoard.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={SignIn} />
+        <Route path="/AdminDashboard" component={AdminDashboard} />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
